316d91d4eb6dd40ecc72b2e13fc51b9e6369aaf8
Zeyu Yao <novodoodle@gmail.com>
2022-10-08 12:07:00 +0800

    Update ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,20 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const client = require("../utils/client");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Get the bot\'s ping'),
-	async execute(interaction) {
-        await interaction.reply(`Pong! ${client.ws.ping}ms`);
-	},
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with \"pong!\" and latency"),
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setColor("#D4AFB9")
+      .setTitle("Pong!")
+      .setDescription(
+        `Roundtrip latency is ${
+          Date.now() - interaction.createdTimestamp
+        }ms. Websocket heartbeat is ${Math.round(client.ws.ping)}ms`
+      )
+
+    await interaction.reply({ embeds: [embed] });
+  },
 };
