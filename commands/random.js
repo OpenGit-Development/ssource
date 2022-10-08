931241bf552d586252aa95c4a575952b2b897409
Zeyu Yao <novodoodle@gmail.com>
2022-10-08 12:45:55 +0800

    Add fields to random command

--- commands/random.js
+++ commands/random.js
@@ -7,7 +7,6 @@ module.exports = {
     .setDescription("Get a random repository on GitHub"),
 
   async execute(interaction) {
-    await interaction.deferReply();
     const randomRepository = await getRandomRepository();
     const info = await getRepositoryInfo(
       randomRepository.owner.login,
@@ -18,10 +17,21 @@ module.exports = {
       .setColor("#171515")
       .setTitle(info.full_name)
       .setDescription(info.description)
-      
+
+      // languages
+      .addFields(
+        { name: "Languages", value: info.language, inline: true },
+        {
+          name: "Stars",
+          value: info.stargazers_count.toString(),
+          inline: true,
+        },
+        { name: "Forks", value: info.forks_count.toString(), inline: true }
+      )
+
       .setURL(info.html_url)
       .setThumbnail(info.owner.avatar_url);
 
-    await interaction.editReply({ embeds: [embed] });
+    await interaction.reply({ embeds: [embed] });
   },
 };
